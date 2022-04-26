5771ecfc6020ad9168e135be5a933611ac50bfbd
Ashley Ruglys <ashley.ruglys@gmail.com>
2022-04-26 20:36:53 +0200

    Make the module global (still technically unsafe but nobody is really using threads in wasm yet)

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,54 @@
 use wasm_bindgen::prelude::wasm_bindgen;
+use once_cell::sync::OnceCell;
 
 #[wasm_bindgen(module = "/basis_universal/webgl/transcoder/build/basis_transcoder.js")]
 extern "C" {
     #[wasm_bindgen(js_name = default)]
-    pub async fn init(map: wasm_bindgen::JsValue) -> wasm_bindgen::JsValue;
+    async fn init(map: wasm_bindgen::JsValue) -> wasm_bindgen::JsValue;
 
     #[wasm_bindgen]
-    pub type Module;
+    type Module;
 
     #[wasm_bindgen(js_name = initializeBasis, method)]
-    pub fn initialize_basis(this: &Module);
+    fn initialize_basis(this: &Module);
 
     #[wasm_bindgen(js_name = BasisFile, method, getter)]
-    pub fn basis_file_constructor(this: &Module) -> js_sys::Function;
+    fn basis_file_constructor(this: &Module) -> js_sys::Function;
+}
+
+struct ModuleWrapper(Module);
+
+impl std::fmt::Debug for Module {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Module")
+    }
+}
+
+// Wasm doesn't use threads... for the most part.
+// Todo: update when that changes.
+unsafe impl Send for ModuleWrapper {}
+unsafe impl Sync for ModuleWrapper {}
+
+static MODULE: OnceCell<ModuleWrapper> = OnceCell::new();
+
+pub async fn wasm_init() -> Result<(), ()> {
+    let module = Module::new().await;
+
+    web_sys::console::log_1(&module);
+
+    MODULE.set(ModuleWrapper(module)).map_err(drop)
+}
+
+pub fn initialize_basis() {
+    MODULE.get().unwrap().0.initialize_basis();
 }
 
 impl Module {
-    pub async fn new() -> Self {
+    async fn new() -> Self {
         Self::new_with_wasm_bytes(include_bytes!("../basis_universal/webgl/transcoder/build/basis_transcoder.wasm")).await
     }
 
-    pub async fn new_with_wasm_bytes(
+    async fn new_with_wasm_bytes(
         bytes: &[u8]
     ) -> Self {
         let array = unsafe {
@@ -33,7 +61,7 @@ impl Module {
         init(map.into()).await.into()
     }
 
-    pub fn create_basis_file(&self, array: &js_sys::Uint8Array) -> BasisFile {
+    fn create_basis_file(&self, array: &js_sys::Uint8Array) -> BasisFile {
         js_sys::Reflect::construct(&self.basis_file_constructor(), &js_sys::Array::of1(&array)).unwrap().into()
     }
 }
@@ -42,9 +70,6 @@ impl Module {
 extern "C" {
     #[wasm_bindgen]
     pub type BasisFile;
-
-    #[wasm_bindgen(constructor)]
-    fn new(array: &js_sys::Uint8Array) -> BasisFile;
 
     #[wasm_bindgen(js_name = getNumImages, method)]
     pub fn get_num_images(this: &BasisFile) -> u32;
@@ -88,4 +113,10 @@ extern "C" {
 
     #[wasm_bindgen(method)]
     pub fn delete(this: &BasisFile);
+}
+
+impl BasisFile {
+    pub fn new(array: &js_sys::Uint8Array) -> Self {
+        MODULE.get().unwrap().0.create_basis_file(array)
+    }
 }
