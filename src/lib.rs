009a82ceb80269d44a8419e483bd13a437e2dcd1
Ashley Ruglys <ashley.ruglys@gmail.com>
2022-05-07 19:36:08 +0200

    Add transcode_uastc_slice

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use wasm_bindgen::prelude::wasm_bindgen;
 use once_cell::sync::OnceCell;
+use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen(module = "/basis_universal/webgl/transcoder/build/basis_transcoder.js")]
 extern "C" {
@@ -14,6 +14,26 @@ extern "C" {
 
     #[wasm_bindgen(js_name = BasisFile, method, getter)]
     fn basis_file_constructor(this: &Module) -> js_sys::Function;
+
+    #[wasm_bindgen(js_name = transcodeUASTCSlice, method)]
+    fn transcode_uastc_slice(
+        this: &Module,
+        dst_blocks: &js_sys::Uint8Array,
+        num_blocks_x: u32,
+        num_blocks_y: u32,
+        image_data: &js_sys::Uint8Array,
+        fmt_int: i32,
+        output_block_or_pixel_stride_in_bytes: u32,
+        bc1_allow_threecolor_blocks: bool,
+        has_alpha: bool,
+        orig_width: u32,
+        orig_height: u32,
+        output_row_pitch_in_blocks_or_pixels: u32,
+        output_rows_in_pixels: u32,
+        channel0: i32,
+        channel1: i32,
+        decode_flags: u32,
+    ) -> bool;
 }
 
 struct ModuleWrapper(Module);
@@ -39,21 +59,56 @@ pub async fn wasm_init() -> Result<(), ()> {
     MODULE.set(ModuleWrapper(module)).map_err(drop)
 }
 
+pub fn transcode_uastc_slice(
+    dst_blocks: &js_sys::Uint8Array,
+    num_blocks_x: u32,
+    num_blocks_y: u32,
+    image_data: &js_sys::Uint8Array,
+    fmt_int: i32,
+    output_block_or_pixel_stride_in_bytes: u32,
+    bc1_allow_threecolor_blocks: bool,
+    has_alpha: bool,
+    orig_width: u32,
+    orig_height: u32,
+    output_row_pitch_in_blocks_or_pixels: u32,
+    output_rows_in_pixels: u32,
+    channel0: i32,
+    channel1: i32,
+    decode_flags: u32,
+) -> bool {
+    MODULE.get().unwrap().0.transcode_uastc_slice(
+        dst_blocks,
+        num_blocks_x,
+        num_blocks_y,
+        image_data,
+        fmt_int,
+        output_block_or_pixel_stride_in_bytes,
+        bc1_allow_threecolor_blocks,
+        has_alpha,
+        orig_width,
+        orig_height,
+        output_row_pitch_in_blocks_or_pixels,
+        output_rows_in_pixels,
+        channel0,
+        channel1,
+        decode_flags,
+    )
+}
+
 pub fn initialize_basis() {
     MODULE.get().unwrap().0.initialize_basis();
 }
 
 impl Module {
     async fn new() -> Self {
-        Self::new_with_wasm_bytes(include_bytes!("../basis_universal/webgl/transcoder/build/basis_transcoder.wasm")).await
+        Self::new_with_wasm_bytes(include_bytes!(
+            "../basis_universal/webgl/transcoder/build/basis_transcoder.wasm"
+        ))
+        .await
     }
 
-    async fn new_with_wasm_bytes(
-        bytes: &[u8]
-    ) -> Self {
-        let array = unsafe {
-            js_sys::Uint8Array::view(bytes)
-        };
+    async fn new_with_wasm_bytes(bytes: &[u8]) -> Self {
+        let array = unsafe { js_sys::Uint8Array::view(bytes) };
 
         let map = js_sys::Object::new();
         js_sys::Reflect::set(&map, &"wasmBinary".into(), &array.into()).unwrap();
@@ -62,7 +117,9 @@ impl Module {
     }
 
     fn create_basis_file(&self, array: &js_sys::Uint8Array) -> BasisFile {
-        js_sys::Reflect::construct(&self.basis_file_constructor(), &js_sys::Array::of1(&array)).unwrap().into()
+        js_sys::Reflect::construct(&self.basis_file_constructor(), &js_sys::Array::of1(&array))
+            .unwrap()
+            .into()
     }
 }
 
